0bdeaa124cdb47492d2d7ed21d9b62f6dfd55030
Stéphan Mestach <smestach@bluesquare.org>
2021-12-10 19:55:35 +0100

    Disable popup when lots of points, move some processing in useEffect

--- src/OrgunitMap.js
+++ src/OrgunitMap.js
@@ -7,7 +7,7 @@ import {
   Popup,
   TileLayer,
   GeoJSON,
-  FeatureGroup
+  FeatureGroup,
 } from "react-leaflet";
 import * as L from "leaflet";
 import "leaflet/dist/leaflet.css";
@@ -31,24 +31,26 @@ const getCircularReplacer = () => {
   };
 };
 
+const shapesFeatureType = ["LineString", "Polygon", "MultiPolygon"];
+
 const maps = [
   {
     name: "Thunderforest - Landscapes",
     attribution: "Thunderforest and OpenStreetMap contributors.",
     url:
-      "https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}{r}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d"
+      "https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}{r}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d",
   },
   {
     name: "Thunderforest - Outdoors",
     attribution: "Thunderforest and OpenStreetMap contributors.",
     url:
-      "https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}{r}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d"
+      "https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}{r}.png?apikey=7c352c8ff1244dd8b732e349e0b0fe8d",
   },
   {
     name: "Google maps - Satelite",
     url: "http://mt3.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-    attribution: "Google"
-  }
+    attribution: "Google",
+  },
 ];
 
 function isString(r) {
@@ -69,19 +71,22 @@ function OrgunitMap({
   showableMap,
   width,
   height,
-  showLayers
+  showLayers,
 }) {
   const [clicked, setClicked] = useState("");
   const [selectedLayer, setSelectedLayer] = useState(maps[0]);
+  const [rawPoints, setRawPoints] = React.useState(undefined);
+
+  const [rawGeojsons, setRawGeojsons] = React.useState(undefined);
   const mapRef = useRef(null);
   const handleClick = () => {
     if (mapRef && mapRef.current) {
       const map = mapRef.current.leafletElement;
       const bounds = Object.values(map._targets)
         .filter((l, index) => (l.getBounds || l.getLatLng) && index > 0)
-        .map(l => (l.getBounds ? l.getBounds() : l.getLatLng().toBounds(10)));
+        .map((l) => (l.getBounds ? l.getBounds() : l.getLatLng().toBounds(10)));
       const bound = bounds[0];
-      bounds.forEach(b => bound.extend(b));
+      bounds.forEach((b) => bound.extend(b));
       if (bound) {
         map.fitBounds(bound);
       }
@@ -92,12 +97,56 @@ function OrgunitMap({
       handleClick();
     }, 1000);
   }, [mapRef]);
+
+  useEffect(() => {
+    const newRawPoints = lines.filter((l) => {
+      if (
+        l.coordinates === undefined &&
+        l.coordinate === undefined &&
+        l.geometry === undefined
+      ) {
+        return false;
+      }
+      const isEventCoordinates =
+        l.coordinate && l.coordinate.latitude && l.coordinate.longitude;
+      if (isEventCoordinates) {
+        return true;
+      }
+      if (l.geometry && l.geometry.type == "Point") {
+        return true;
+      }
+      if (l.coordinates === undefined) {
+        return false;
+      }
+      return (
+        isString(l.coordinates) &&
+        !l.coordinates.startsWith("[[") &&
+        l.coordinates != ""
+      );
+    });
+
+    const newGeojsons = lines.filter(
+      (l) =>
+        (l.coordinates &&
+          isString(l.coordinates) &&
+          l.coordinates.startsWith("[[")) ||
+        (l.geometry &&
+          l.geometry.type &&
+          shapesFeatureType.includes(l.geometry.type))
+    );
+    setRawGeojsons(newGeojsons);
+    setRawPoints(newRawPoints);
+  }, [lines]);
   if (lines == undefined || lines == null) {
     return <></>;
   }
 
   if (!showableMap) {
-    return <p>Map will show if the lines contains a 'coordinates' or 'geometry' field</p>;
+    return (
+      <p>
+        Map will show if the lines contains a 'coordinates' or 'geometry' field
+      </p>
+    );
   }
 
   function onFeature(feature, event) {
@@ -113,7 +162,7 @@ function OrgunitMap({
     // hide the target node
     removed = {
       node: currentTarget,
-      pointerEvents: currentTarget.style.pointerEvents
+      pointerEvents: currentTarget.style.pointerEvents,
     };
     currentTarget.style.pointerEvents = "none";
 
@@ -142,126 +191,104 @@ function OrgunitMap({
     setClicked(feature);
   }
 
-  const geojsons = lines
-    .filter(
-      l =>
-        (l.coordinates &&
-          isString(l.coordinates) &&
-          l.coordinates.startsWith("[[")) ||
-        (l.geometry &&
-          l.geometry.type &&
-          ["LineString", "Polygon", "MultiPolygon"].includes(l.geometry.type))
-    )
-    .map((line, index) => {
-      let geometry = line.geometry;
-      try {
-        geometry = turf.polygon(JSON.parse(line.coordinates));
-      } catch (ignored) {
-        try {
-          geometry = turf.multiPolygon(JSON.parse(line.coordinates));
-        } catch (ignored) {}
-      }
-      if (geometry) {
-        if (geometry.properties) {
-          geometry.properties.line = line;
-        }
-      }
-      const opacity = geometry.type == "LineString" ? 1 : 0.3;
-      line.fillColor = line.fillColor || getRandomColor();
-      const style = {
-        fillColor: line.fillColor,
-        color: line.color || getRandomColor(),
-        weight: line.opacity || opacity,
-        opacity: line.opacity || opacity,
-        fillOpacity: line.opacity || opacity
-      };
-      if (clicked == line) {
-        style.weight = 3;
-        style.opacity = 0.8;
-        style.dashArray = "5,5";
-      }
-      return (
-        <GeoJSON
-          data={geometry}
-          key={"parent-" + index + (clicked == line ? "clicked" : "none")}
-          style={style}
-          title={JSON.stringify(line, getCircularReplacer())}
-          onClick={event => {
-            onFeature(line, event);
-          }}
-        />
-      );
-    });
+  const geojsons =
+    rawGeojsons == undefined
+      ? []
+      : rawGeojsons.map((line, index) => {
+          let geometry = line.geometry;
+          try {
+            geometry = turf.polygon(JSON.parse(line.coordinates));
+          } catch (ignored) {
+            try {
+              geometry = turf.multiPolygon(JSON.parse(line.coordinates));
+            } catch (ignored) {}
+          }
+          if (geometry) {
+            if (geometry.properties) {
+              geometry.properties.line = line;
+            }
+          }
+          const opacity = geometry.type == "LineString" ? 1 : 0.3;
+          line.fillColor = line.fillColor || getRandomColor();
+          const style = {
+            fillColor: line.fillColor,
+            color: line.color || getRandomColor(),
+            weight: line.opacity || opacity,
+            opacity: line.opacity || opacity,
+            fillOpacity: line.opacity || opacity,
+          };
+          if (clicked == line) {
+            style.weight = 3;
+            style.opacity = 0.8;
+            style.dashArray = "5,5";
+          }
+          return (
+            <GeoJSON
+              data={geometry}
+              key={"parent-" + index + (clicked == line ? "clicked" : "none")}
+              style={style}
+              title={JSON.stringify(line, getCircularReplacer())}
+              onClick={(event) => {
+                onFeature(line, event);
+              }}
+            />
+          );
+        });
 
-  const points = lines
-    .filter(l => {
-      if (
-        l.coordinates === undefined &&
-        l.coordinate === undefined &&
-        l.geometry === undefined
-      ) {
-        return false;
-      }
-      const isEventCoordinates =
-        l.coordinate && l.coordinate.latitude && l.coordinate.longitude;
-      if (isEventCoordinates) {
-        return true;
-      }
-      if (l.geometry && l.geometry.type == "Point") {
-        return true;
-      }
-      if (l.coordinates === undefined) {
-        return false;
-      }
-      return (
-        isString(l.coordinates) &&
-        !l.coordinates.startsWith("[[") &&
-        l.coordinates != ""
-      );
-    })
-    .map((line, index) => {
-      const latlong =
-        line.coordinate && line.coordinate.latitude && line.coordinate.longitude
-          ? [line.coordinate.latitude, line.coordinate.longitude]
-          : line.coordinates
-          ? JSON.parse(line.coordinates).reverse()
-          : line.geometry && line.geometry.coordinates.slice(0).reverse();
-      return (
-        <CircleMarker
-          key={index}
-          radius={2}
-          center={latlong}
-          color={line.color || "red"}
-          title={index}
-        >
-          <Popup>
-            {Object.keys(line).map(k => {
-              return (
-                <div>
-                  <b>{k}</b> <AsPrimitive value={line[k]} />
-                </div>
-              );
-            })}
-          </Popup>
-        </CircleMarker>
-      );
-    });
+  const radius = rawPoints && rawPoints.length < 1000 ? 2 : 1;
+  const displayPopup = rawPoints && rawPoints.length < 10000 ? true : false;
+
+  const points =
+    rawPoints == undefined
+      ? []
+      : rawPoints.map((line, index) => {
+          const latlong =
+            line.coordinate &&
+            line.coordinate.latitude &&
+            line.coordinate.longitude
+              ? [line.coordinate.latitude, line.coordinate.longitude]
+              : line.coordinates
+              ? JSON.parse(line.coordinates).reverse()
+              : line.geometry && line.geometry.coordinates.slice(0).reverse();
+          return (
+            <CircleMarker
+              key={index}
+              radius={radius}
+              center={latlong}
+              color={line.color || "red"}
+              title={index}
+            >
+              {displayPopup && (
+                <Popup>
+                  {Object.keys(line).map((k) => {
+                    return (
+                      <div>
+                        <b>{k}</b> <AsPrimitive value={line[k]} />
+                      </div>
+                    );
+                  })}
+                </Popup>
+              )}
+            </CircleMarker>
+          );
+        });
   const mapSelected = (event, val) => {
     setSelectedLayer(val.props.value);
   };
 
   return (
     <div className="avoid-page-break">
+      <p>{displayPopup ? "popup enabled" : "popup disabled"}</p>
       {showLayers && (
         <div
           style={{
-            display: "flex"
+            display: "flex",
           }}
         >
           <FormControl>
             <InputLabel>Layer</InputLabel>
             <Select onChange={mapSelected} value={selectedLayer}>
-              {maps.map(m => (
+              {maps.map((m) => (
                 <MenuItem value={m}>{m.name}</MenuItem>
               ))}
             </Select>
@@ -274,8 +301,8 @@ function OrgunitMap({
         {geojsons.length} zones displayed.{" "}
         {clicked &&
           Object.keys(clicked)
-            .filter(k => !["geometry", "coordinates"].includes(k))
-            .map(k => {
+            .filter((k) => !["geometry", "coordinates"].includes(k))
+            .map((k) => {
               return (
                 <div>
                   <b>{k}</b> <AsPrimitive value={clicked[k]} />
@@ -285,6 +312,7 @@ function OrgunitMap({
       </div>
 
       <Map
+        preferCanvas={true}
         doubleClickZoom={false}
         center={position}
         zoom={3}
@@ -292,7 +320,7 @@ function OrgunitMap({
         style={{
           width: width || "80%",
           height: height || "900px",
-          padding: "0px"
+          padding: "0px",
         }}
       >
         <TileLayer {...selectedLayer}></TileLayer>
