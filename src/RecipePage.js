28d0c1c4f3b5dc0022400302c785be0bc394f19b
Stéphan Mestach <smestach@bluesquare.org>
2022-04-19 10:07:34 +0200

    Fix encoding on old dhis2 version, display only errors on recipe save, make sure the error is show even if no message

--- src/RecipePage.js
+++ src/RecipePage.js
@@ -5,6 +5,7 @@ import { useQueryClient, useQuery } from "react-query";
 import builtInRecipes from "./recipes";
 import { useMutation } from "react-query";
 import RecipeAlert from "./RecipeAlert";
+import { fixChars } from "./support/fixChars";
 
 function useDefaultQuery() {
   return new URLSearchParams(useLocation().search);
@@ -14,12 +15,20 @@ const RecipePage = (props) => {
   const { match, editable, dhis2, freshRecipe } = props;
   const query = useDefaultQuery();
   const autorun = query.get("autorun") === "true";
-  const recipeId = match.params.recipeId
+  const recipeId = match.params.recipeId;
   const queryClient = useQueryClient();
   const [alertMessage, setAlertMessage] = useState(undefined);
   const onSaveMutation = useMutation(
     async ({ modifiedRecipe }) => {
       const api = await dhis2.api();
+      const versionInfo = await api.get("system/info");
+      if (
+        versionInfo.version === "2.30" ||
+        versionInfo.version === "2.29" ||
+        versionInfo.version === "2.28"
+      ) {
+        modifiedRecipe.code = fixChars(modifiedRecipe.code);
+      }
       try {
         const createResp = await api.post(
           "/dataStore/taskr/" + modifiedRecipe.id,
@@ -37,43 +46,36 @@ const RecipePage = (props) => {
       onSuccess: () => {
         queryClient.invalidateQueries("loadRecipes");
         queryClient.invalidateQueries("fetchRecipe");
-        setAlertMessage("Recipe saved successfully");
+        setAlertMessage(undefined);
       },
       onError: (error) => {
+        // not always an error, sometime just a string (ex page not found)
         console.log(error);
-        setAlertMessage(error.message);
-      }
+        setAlertMessage("The recipe couldn't be saved : " + error.message);
+      },
     }
-  )
-  
+  );
+
   const isError = onSaveMutation?.isError;
 
-  const fetchRecipeQuery = useQuery(
-    ["fetchRecipe", recipeId],
-    async () => {
-      const api = await dhis2.api();
+  const fetchRecipeQuery = useQuery(["fetchRecipe", recipeId], async () => {
+    const api = await dhis2.api();
 
-      try {
-        const response = await api.get(
-          "/dataStore/taskr/" + recipeId
-        );
-        return response;
-      } catch (ignored) {
-        const defaultRecipe = builtInRecipes.find(
-          (recipe) => recipe.id === recipeId
-        );
-        return defaultRecipe || freshRecipe(recipeId);
-      }
+    try {
+      const response = await api.get("/dataStore/taskr/" + recipeId);
+      return response;
+    } catch (ignored) {
+      const defaultRecipe = builtInRecipes.find(
+        (recipe) => recipe.id === recipeId
+      );
+      return defaultRecipe || freshRecipe(recipeId);
     }
- );
+  });
 
   return (
     <div>
-      <br/>
-      <RecipeAlert 
-        message={alertMessage}
-        isError={isError}
-      />
+      <br />
+      <RecipeAlert message={alertMessage} isError={isError} />
 
       {fetchRecipeQuery.data && (
         <Editor
