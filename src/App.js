138622fff8cd364a0e118ba93516005ceb82678f
Stéphan Mestach <smestach@bluesquare.org>
2022-10-12 16:50:53 +0200

    Allow to load gist as recipe

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React, { Suspense } from "react";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography, AppBar, Button, Paper, Toolbar, Grid } from "@material-ui/core"
+import {
+  Typography,
+  AppBar,
+  Button,
+  Paper,
+  Toolbar,
+  Grid,
+} from "@material-ui/core";
 import {
   HashRouter as Router,
   Switch,
@@ -13,8 +20,8 @@ import { generateUid } from "d2/lib/uid";
 import RecipesPage from "./RecipesPage";
 import Dhis2 from "./support/Dhis2";
 import RecipePage from "./RecipePage";
+import RecipeGistPage from "./RecipeGistPage";
 import AppDrawer from "./AppDrawer";
-
 
 const DocPage = React.lazy(() => import("./DocPage"));
 
@@ -77,98 +84,127 @@ function App() {
   return (
     <Router>
       <QueryClientProvider client={queryClient}>
-          <div className={classes.root + " reportPage"}>
-            <AppBar position="static" color="primary" className="no-print">
-              <Toolbar>
-                <Grid
-                  container
-                  justify="space-between"
-                  alignItems="center"
-                  alignContent="center"
-                >
-                  <Grid item>
-                    <AppDrawer classes={classes} />
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="h6" color="inherit">
-                      Taskr : your task runner.
-                    </Typography>
-                  </Grid>
-                  <Grid item>
-                    <Button href={"#/doc/"} color="inherit">
-                      Documentation
-                    </Button>
-                  </Grid>
+        <div className={classes.root + " reportPage"}>
+          <AppBar position="static" color="primary" className="no-print">
+            <Toolbar>
+              <Grid
+                container
+                justify="space-between"
+                alignItems="center"
+                alignContent="center"
+              >
+                <Grid item>
+                  <AppDrawer classes={classes} />
                 </Grid>
-              </Toolbar>
-            </AppBar>
-            <Paper className={classes.paper}>
-              <Switch>
-                <Route
-                  path={`/doc`}
-                  exact={true}
-                  render={(props) => (
-                    <Suspense fallback={<div>Loading...</div>}>
-                      <DocPage match={props.match} />
-                    </Suspense>
-                  )}
-                />
-                <Route
-                  path={`/doc/:section`}
-                  render={(props) => (
-                    <Suspense fallback={<div>Loading...</div>}>
-                      <DocPage match={props.match} />
-                    </Suspense>
-                  )}
-                />
-                <Route
-                  path={`/recipes`}
-                  exact={true}
-                  render={(props) => (
-                    <RecipesPage
-                      dhis2={dhis2}
-                      freshRecipe={freshRecipe}
-                      classes={classes}
-                      history={props.history}
-                      onNewRecipe={onNewRecipe}
-                      match={props.match}
-                    />
-                  )}
-                />
-                <Route
-                  path={`/recipes/:recipeId`}
-                  exact={true}
-                  render={(props) => {
-                    return (
-                      <RecipePage
-                        dhis2={dhis2}
-                        classes={classes}
-                        freshRecipe={freshRecipe}
-                        match={props.match}
-                        history={props.history}
-                        editable={true}
-                      />
-                    );
-                  }}
-                />
-                <Route
-                  path={`/recipes/:recipeId/run`}
-                  render={(props) => (
+                <Grid item>
+                  <Typography variant="h6" color="inherit">
+                    Taskr : your task runner.
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Button href={"#/doc/"} color="inherit">
+                    Documentation
+                  </Button>
+                </Grid>
+              </Grid>
+            </Toolbar>
+          </AppBar>
+          <Paper className={classes.paper}>
+            <Switch>
+              <Route
+                path={`/doc`}
+                exact={true}
+                render={(props) => (
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <DocPage match={props.match} />
+                  </Suspense>
+                )}
+              />
+              <Route
+                path={`/doc/:section`}
+                render={(props) => (
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <DocPage match={props.match} />
+                  </Suspense>
+                )}
+              />
+              <Route
+                path={`/recipes`}
+                exact={true}
+                render={(props) => (
+                  <RecipesPage
+                    dhis2={dhis2}
+                    freshRecipe={freshRecipe}
+                    classes={classes}
+                    history={props.history}
+                    onNewRecipe={onNewRecipe}
+                    match={props.match}
+                  />
+                )}
+              />
+              <Route
+                path={`/recipes/:recipeId`}
+                exact={true}
+                render={(props) => {
+                  return (
                     <RecipePage
                       dhis2={dhis2}
                       classes={classes}
                       freshRecipe={freshRecipe}
                       match={props.match}
                       history={props.history}
-                      editable={false}
+                      editable={true}
                     />
-                  )}
-                />
+                  );
+                }}
+              />
+              <Route
+                path={`/recipes/:recipeId/run`}
+                render={(props) => (
+                  <RecipePage
+                    dhis2={dhis2}
+                    classes={classes}
+                    freshRecipe={freshRecipe}
+                    match={props.match}
+                    history={props.history}
+                    editable={false}
+                  />
+                )}
+              />
 
-                <Redirect to={"/recipes"} />
-              </Switch>
-            </Paper>
-          </div>
+              <Route
+                path={`/gh/g/:repo/:gistId`}
+                exact={true}
+                render={(props) => (
+                  <RecipeGistPage
+                    dhis2={dhis2}
+                    classes={classes}
+                    freshRecipe={freshRecipe}
+                    match={props.match}
+                    history={props.history}
+                    editable={true}
+                  />
+                )}
+              />
+              <Route
+                path={`/gh/g/:repo/:gistId/run`}
+                exact={true}
+                render={(props) => (
+                  <RecipeGistPage
+                    dhis2={dhis2}
+                    classes={classes}
+                    freshRecipe={freshRecipe}
+                    match={props.match}
+                    history={props.history}
+                    editable={false}
+                  />
+                )}
+              />
+
+              <Redirect to={"/recipes"} />
+            </Switch>
+          </Paper>
+        </div>
       </QueryClientProvider>
     </Router>
   );
